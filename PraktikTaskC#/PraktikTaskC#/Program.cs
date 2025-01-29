3b59e21e48707dc92cb13451b9e2822e1e32bfff
Христюк Максим Анатолійович <ipz232_khma@student.ztu.edu.ua>
2025-01-30 01:16:31 +0200

    TechPrakt2025Task0

---
+++ PraktikTaskC#/PraktikTaskC#/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введiть перше число: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введiть друге число: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        double sum = num1 + num2;
+
+        Console.WriteLine($"Сума чисел {num1} i {num2} дорiвнює {sum}");
+    }
+}
